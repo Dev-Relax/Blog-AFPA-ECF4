cdddc2790cae0141f4b44b7db6874fdedc8da9ac
relaxator <1loic.simon@gmail.com>
2024-05-21 11:08:09 +0200

    added boilerplate template

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+require "./src/partials/header.php";
+require "./src/partials/footer.php";
---
+++ src/partials/footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            Blog créé par - 
+        </footer>
+        <script type="text/javascript" src="../../assets/js/scripts.js"></script>
+    </body>
+</html>
