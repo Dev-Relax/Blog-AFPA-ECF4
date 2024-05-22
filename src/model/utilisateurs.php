17d1e96e9e4dd71700e68dcf63875c4854d5436d
relaxator <1loic.simon@gmail.com>
2024-05-22 11:14:30 +0200

    updated Utilisateurs

--- src/model/utilisateurs.php
+++ src/model/utilisateurs.php
@@ -1,34 +1,51 @@
 <?php
 require_once 'db.php';
-function verifierUtilisateur($pseudo) { 
-try {
 
-    $bdd = new PDO($dsn, $user, $password);
-} catch (PDOException $Exception) {
-    echo 'Échec lors de la connexion : ' . $Exception->getMessage();
-}
 
-   // 1 preparer la requete
+
+function verifierUtilisateur($pseudo)
+{
+    try {
+        $bdd = connectToBdd();
+    } catch (PDOException $Exception) {
+        echo 'Échec lors de la connexion : ' . $Exception->getMessage();
+    }
+    
+    // 1 preparer la requete
     $query = $bdd->prepare('SELECT * FROM utilisateurs WHERE pseudo = ?');
-     $pseudo = $_POST['pseudo'];
     // 2 executer la requete
     $query->execute([$pseudo]);
     $users = $query->fetchAll(PDO::FETCH_ASSOC);
+    return ($users > 0) ? true : false;
 }
 
 //ajouter un utilisateur
-function ajoutUtilisateur {
-// Vérifiez si les données POST nécessaires sont définies
-if (isset($_POST['nom'], $_POST['prenom'], $_POST['pseudo'], $_POST['mail'], $_POST['mdp'])) {
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $pseudo = $_POST['pseudo'];
-    $mail = $_POST['mail'];
-    $mdp = $_POST['mdp'];
+function ajoutUtilisateur()
+{
 
-    // Préparation de la requête SQL
-    $query = $bdd->prepare('INSERT INTO utilisateurs (nom, prenom, pseudo, mail, mdp) VALUES (?, ?, ?, ?, ?)');
+    try {
+        $bdd = connectToBdd();
+    } catch (PDOException $Exception) {
+        echo 'Échec lors de la connexion : ' . $Exception->getMessage();
+    }
 
-    // Exécution de la requête avec les valeurs fournies
-    $result = $query->execute([$nom, $prenom, $pseudo, $mail, $mdp]);
+    // Vérifiez si les données POST nécessaires sont définies
+    if (isset($_POST['nom'], $_POST['prenom'], $_POST['pseudo'], $_POST['mail'], $_POST['mdp'])) {
+
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $pseudo = $_POST['pseudo'];
+        $mail = $_POST['mail'];
+        $mdp = $_POST['mdp'];
+
+        if (!verifierUtilisateur($pseudo)) {
+
+            $hash = password_hash($mdp, PASSWORD_BCRYPT);
+            // Préparation de la requête SQL
+            $query = $bdd->prepare('INSERT INTO utilisateurs (nom, prenom, pseudo, mail, mdp) VALUES (?, ?, ?, ?, ?)');
+
+            // Exécution de la requête avec les valeurs fournies
+            $result = $query->execute([$nom, $prenom, $pseudo, $mail, $mdp]);
+        }
+    }
 }
